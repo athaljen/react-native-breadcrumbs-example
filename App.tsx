36be7fc3353a68ed6e5c4d69af546e1b3eb61a6a
athaljen <athaljen@gmail.com>
2024-04-17 22:12:12 +0530

    feat: add navigation and manage breadcrum strecture and navigation

--- App.tsx
+++ App.tsx
@@ -1,118 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+'use strict;';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
+  NavigationContainer,
+  NavigationState,
+  RouteProp,
+} from '@react-navigation/native';
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack';
+import React, {memo, useCallback, useEffect, useMemo, useRef} from 'react';
+import {Pressable, ScrollView, StyleSheet, Text, View} from 'react-native';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+type StackParamList = {Home: {name: string}};
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+type StackScreenProps<S extends keyof StackParamList> = {
+  route: RouteProp<StackParamList, S>;
+  navigation: NativeStackNavigationProp<StackParamList, S>;
+};
+
+const Stack = createNativeStackNavigator<StackParamList>();
+
+const Home = ({route, navigation}: StackScreenProps<'Home'>) => {
+  const scrollRef = useRef<ScrollView>(null);
+
+  useEffect(() => {
+    scrollRef.current?.scrollToEnd({animated: false});
+    if (route.params?.name) navigation.setOptions({title: route.params?.name});
+  }, [navigation, route.params?.name]);
+
+  const Name = useMemo(() => Math.ceil(Math.random() * 10000), []);
+
+  const NavigationData = useMemo(
+    () => navigation.getState(),
+    [route, navigation],
+  );
+
+  const gotoNewScreen = useCallback(() => {
+    navigation.push('Home', {name: Name.toString()});
+  }, [Name]);
+
+  const navigateToSpecific = useCallback(
+    (item: NavigationState['routes'][0]) => {
+      const targetIndex = NavigationData.routes.findIndex(
+        route => route.key === item.key,
+      );
+
+      if (targetIndex !== -1 && targetIndex < NavigationData.index) {
+        navigation.pop(NavigationData.index - targetIndex);
+      }
+    },
+    [NavigationData],
+  );
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.App}>
+      <View>
+        <ScrollView horizontal ref={scrollRef}>
+          {NavigationData.routes.map((item, index) => (
+            <Text
+              key={index.toString()}
+              style={styles.text}
+              onPress={navigateToSpecific.bind(null, item)}>
+              {item?.params?.name ? item.params?.name : route.name}
+              {'>'}
+            </Text>
+          ))}
+        </ScrollView>
+      </View>
+
+      <Pressable onPress={gotoNewScreen} style={styles.press}>
+        <Text style={styles.text}>{Name}</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const App = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.App}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{animation: 'slide_from_right'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  App: {flex: 1},
+  press: {alignSelf: 'center', marginTop: 100},
+  text: {marginRight: 10, color: '#000000'},
 });
 
-export default App;
+export default memo(App);
